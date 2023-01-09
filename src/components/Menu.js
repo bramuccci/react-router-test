ca02e2b966fb885effedd351ea07ae6223c5e95d
Candela Bramucci <candelabramucci05@gmail.com>
2023-01-09 13:23:54 -0300

    using NavLink

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,37 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export function Menu() {
-    return <p>Menu</p>
+    return (
+        <nav>
+            <ul>
+                {routes.map(route => (
+                    <li key={route.text}>
+                        <NavLink
+                            style={({ isActive }) => ({
+                                color: isActive ? 'green' : 'red',
+                            })}
+                            to={route.to}
+                        >
+                            {route.text}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
 }
+
+const routes = []
+routes.push({
+    to: '/',
+    text: 'Home',
+})
+routes.push({
+    to: '/blog',
+    text: 'blog',
+})
+routes.push({
+    to: '/profile',
+    text: 'profile',
+})
