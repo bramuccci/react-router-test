4051596a936baa07813d86c99b14876d4d99b2bb
Candela Bramucci <candelabramucci05@gmail.com>
2023-01-09 20:50:36 -0300

    rendering menu items depending on auth status

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,22 +1,30 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useAuth } from './auth'
 
 export function Menu() {
+    const auth = useAuth()
+
     return (
         <nav>
             <ul>
-                {routes.map(route => (
-                    <li key={route.text}>
-                        <NavLink
-                            style={({ isActive }) => ({
-                                color: isActive ? 'green' : 'red',
-                            })}
-                            to={route.to}
-                        >
-                            {route.text}
-                        </NavLink>
-                    </li>
-                ))}
+                {routes.map(route => {
+                    if (route.onlyIfNotAuth && auth.user) return null
+                    if (route.private && !auth.user) return null
+
+                    return (
+                        <li key={route.text}>
+                            <NavLink
+                                style={({ isActive }) => ({
+                                    color: isActive ? 'green' : 'red',
+                                })}
+                                to={route.to}
+                            >
+                                {route.text}
+                            </NavLink>
+                        </li>
+                    )
+                })}
             </ul>
         </nav>
     )
@@ -26,12 +34,26 @@ const routes = []
 routes.push({
     to: '/',
     text: 'Home',
+    private: false,
 })
 routes.push({
     to: '/blog',
-    text: 'blog',
+    text: 'Blog',
+    private: false,
 })
 routes.push({
     to: '/profile',
-    text: 'profile',
+    text: 'Profile',
+    private: true,
+})
+routes.push({
+    to: '/login',
+    text: 'Login',
+    private: false,
+    onlyIfNotAuth: true,
+})
+routes.push({
+    to: '/logout',
+    text: 'Log out',
+    private: true,
 })
