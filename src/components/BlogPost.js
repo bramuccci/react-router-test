3ad31b823aa31fdccbd4760db9f3a027b7a7db95
Candela Bramucci <candelabramucci05@gmail.com>
2023-01-09 16:53:07 -0300

    return button in blogpost

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { blogData } from '../data/blogdata'
 
 export function BlogPost() {
+    const navigate = useNavigate()
     const { slug } = useParams()
 
     const blogPost = blogData.find(post => post.slug === slug)
 
+    const returnToBlog = () => {
+        navigate('/blog')
+    }
+
     return (
         <>
+            <button onClick={returnToBlog}>return to blog</button>
             <h2>{blogPost.title}</h2>
             <li>{blogPost.author}</li>
             <p>{blogPost.content}</p>
